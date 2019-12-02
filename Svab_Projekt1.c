61cdd8d018a6892960b0bad5b766afb2ec18e341
Martin Svab <martin.svab@gmail.com>
2019-12-02 20:43:31 +0100

    bola odstranena funkcia pracovalRok

--- Svab_Projekt1.c
+++ Svab_Projekt1.c
@@ -4,6 +4,8 @@
 
 #define DLZKA_MENA 50
 #define DLZKA_SPZ 7
+#define ODMENA_STARE 5.1
+#define ODMENA_NOVE 2.3
 
 int otvorSubor(FILE **vstupnySubor) {
     //ak sa súbor ešte nebol otvorený, tak sa ho pokúsi otvoriť
@@ -66,65 +68,15 @@ void vypisSuboru(FILE **vstupnySubor) {
     }
 }
 
-int pracovalRok(char *datum, char *aktualnyDatum) {
-    int i;
-    int rok, mesiac, den;
-    int aktualnyRok, aktualnyMesiac, aktualnyDen;
-    char rokStr[5], mesiacStr[3], denStr[3];
-    char aktualnyRokStr[5], aktualnyMesiacStr[3], aktualnyDenStr[3];
-    for (i = 0; i < 4; i++) {
-        rokStr[i] = datum[i];
-        aktualnyRokStr[i] = aktualnyDatum[i];
-    }
-    rokStr[4] = '\0';
-    aktualnyRokStr[4] = '\0';
-    for (i = 0; i < 2; i++) {
-        mesiacStr[i] = datum[i+4];
-        aktualnyMesiacStr[i] = aktualnyDatum[i+5];
-    }
-    mesiacStr[2] = '\0';
-    aktualnyMesiacStr[2] = '\0';
-    for (i = 0; i < 2; i++) {
-        denStr[i] = datum[i+6];
-        aktualnyDenStr[i] = aktualnyDatum[i+7];
-    }
-    denStr[2] = '\0';
-    aktualnyDenStr[2] = '\0';
-    rok = atoi(rokStr);
-    aktualnyRok = atoi(aktualnyRokStr);
-    mesiac = atoi(mesiacStr);
-    aktualnyMesiac = atoi(aktualnyMesiacStr);
-    den = atoi(denStr);
-    aktualnyDen = atoi(aktualnyDenStr);
-    if (aktualnyRok - rok > 1) {
-        return 1;
-    } else if (aktualnyRok - rok == 1) {
-        if (aktualnyMesiac > mesiac) {
-            return 1;
-        } else if (aktualnyMesiac = mesiac) {
-            if (aktualnyDen >= den) {
-                return 1;
-            } else {
-                return 0;
-            }
-        } else {
-            return 0;
-        }
-    } else {
-        return 0;
-    }
-}
-
 void odmena(FILE **fr) {
     if (*fr != NULL) {
-        #define ODMENA_STARE 5.1
-        #define ODMENA_NOVE 2.3
-
-        char aktualnyDatum[9], datum[9], menoPriezvisko[51], spz[8];
+        int aktualnyDatum, datumZamestnania;
+        char menoPriezvisko[DLZKA_MENA + 1]
+        char spz[DLZKA_SPZ + 1];
         int typAuta;
         double cena, odmena;
 
-        scanf("%s", aktualnyDatum);
+        scanf("%s", datum);
         rewind(*fr);
         while(fgets(menoPriezvisko, 51, *fr) != NULL) {
             if (menoPriezvisko[strlen(menoPriezvisko) - 1] == '\n') {
